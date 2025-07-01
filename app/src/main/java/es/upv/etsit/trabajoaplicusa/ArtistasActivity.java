b8eb63785078b1baece4e8375358b84798dde241
OLMORBRI <olmorbri@upv.edu.es>
2025-07-01 21:12:31 +0200

    Pantalla principal y Eventos creados

---
+++ app/src/main/java/es/upv/etsit/trabajoaplicusa/ArtistasActivity.java
@@ -0,0 +1,12 @@
+package es.upv.etsit.trabajoaplicusa;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ArtistasActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artistas);  // Asegúrate de tener ese layout
+    }
+}
--- app/src/main/java/es/upv/etsit/trabajoaplicusa/MainActivity.java
+++ app/src/main/java/es/upv/etsit/trabajoaplicusa/MainActivity.java
@@ -1,6 +1,9 @@
 package es.upv.etsit.trabajoaplicusa;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,48 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnEventos, btnArtistas, btnInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Referencias a los botones
+        btnEventos = findViewById(R.id.btnEventos);
+        btnArtistas = findViewById(R.id.btnArtistas);
+        btnInfo = findViewById(R.id.btnInfo);
+
+        // Listeners
+        btnEventos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EventosActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnArtistas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ArtistasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
